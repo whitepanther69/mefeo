37609fa96a9aa438cd61864dbcd5340a8bd1b3a8
whitepanther69 <159169210+whitepanther69@users.noreply.github.com>
2025-05-18 18:50:15 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -14,7 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
     "assets/video/video1.mp4",
     "assets/video/video2.mp4",
     "assets/video/video3.mp4",
-    "assets/video/video4.mp4"
+    "assets/video/video4.mp4",
+    "assets/video/video5.mp4"
   ];
   let index = 0;
 
